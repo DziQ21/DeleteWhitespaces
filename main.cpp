5fe93ca739c24293a08cba61fc141f8b4e9e8a3d
Paweł Radzik <42906156+DziQ21@users.noreply.github.com>
2022-04-28 14:42:20 +0200

    edge case scenario

--- main.cpp
+++ main.cpp
@@ -4,25 +4,31 @@
 
 // trim from start (in place)
 std::string::iterator findFirstLetter(std::string &s) {
-    return std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+    return std::find_if(s.begin(), s.end(), [](char ch) {
         return !std::isspace(ch);
     });
 }
 
 // trim from end (in place)
 std::string::iterator findLastLetter(std::string &s) {
-    return std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+    return std::find_if(s.rbegin(), s.rend(), [](char ch) {
         return !std::isspace(ch);
     }).base();
 }
 std::string deleteWhitespaces(std::string &s) {
-    std::string res(findFirstLetter(s),findLastLetter(s));
-    return res;
+    auto firstCharacter=findFirstLetter(s);
+    auto lastCharacter=findLastLetter(s);
+    if (firstCharacter<lastCharacter)//when there is no letter problem may occur
+        return std::string(findFirstLetter(s),findLastLetter(s));
+    return "";
 }
 
 int main() {
     std::string a = "\t\t\n this is example \tstring \t \n \t ";
+    std::string b = "\t\t\n \t \n \t ";
+
     std::cout<<deleteWhitespaces(a);
+    std::cout<<deleteWhitespaces(b);
 }
 
 
